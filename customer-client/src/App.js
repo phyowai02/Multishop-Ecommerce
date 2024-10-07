e69264254d4cbbb8dd7d414533d31b35677350c3
phyowai <heinhtetphyowai71@gmail.com>
2024-10-07 16:44:21 +0630

    changes commit

--- customer-client/src/App.js
+++ customer-client/src/App.js
@@ -5,18 +5,18 @@ import ProductDetail from "./components/ProductDetail";
 import Login from "./components/Login";
 import CartDisplay from "./components/CartDisplay";
 import OrderSuccess from "./components/OrderSuccess";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/homepage' element={<HomePage/>}/>
-        <Route path='/product' element={<Product/>}/>
-        <Route path='/productdetail' element={<ProductDetail/>}/>
-        <Route path='/' element={<Login/>} />
-        <Route path='/cartdisplay' element={<CartDisplay/>} />
-        <Route path='/success' element={<OrderSuccess/>} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productdetail" element={<ProductDetail />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/cartdisplay" element={<CartDisplay />} />
+        <Route path="/success" element={<OrderSuccess />} />
       </Routes>
     </BrowserRouter>
   );
